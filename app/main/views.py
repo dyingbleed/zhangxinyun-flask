966beb1cb8c8819797633122e2d6c8efad001329
Zhen Li <anthony@rmbp.local>
2015-12-14 21:53:41 +0800

    动态导航栏

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,13 @@
+#coding:utf-8
 from flask import render_template
 
 from . import main
 
 @main.route('/')
 def index():
-    return render_template("index.html", title=u"\u5f20\u99a8\u5141\u7684\u4e3b\u9875")
+    return render_template("index.html", 
+    	title=u"\u5f20\u99a8\u5141\u7684\u4e3b\u9875", 
+    	menus=[
+    	{'title': u"\u6570\u636e\u91c7\u96c6", 'url': "/scrapy"},
+    	{'title': u"\u6570\u636e\u5904\u7406", 'url': "/spark"}
+    	])
