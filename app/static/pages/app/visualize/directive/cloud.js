793b6ef2e6bd57e18be7af0dd6aade2eb90b4560
Anthony Li <lizhen.dgut@gmail.com>
2016-01-03 13:05:15 +0800

    标签云指令

---
+++ app/static/pages/app/visualize/directive/cloud.js
@@ -0,0 +1,54 @@
+define(function(require) {
+	var d3 = require('d3');
+	require('d3-layout-cloud');
+	
+	var modules = require('modules');
+
+	var visualize = modules.get('visualize');
+	visualize.directive('zxyCloud', function() {
+		return {
+			restrict: 'A',
+			scope: {
+				data: '='
+			},
+			link: function(scope, element, attrs) {
+				var width = element.width();
+				var height = element.height();
+
+				var fill = d3.scale.category20();
+
+				var layout = d3.layout.cloud();
+				layout.words(scope.data)
+					.size([width, height])
+					.padding(5)
+					.rotate(function() { return 0; })
+					.fontSize(function(i) { return i['size'] })
+					.on('end', draw);
+
+				layout.start();
+
+				function draw(words) {
+					d3.select(element.get(0))
+						.append('svg')
+						.attr('width', width)
+						.attr('height', height)
+						.append('g')
+						.attr('transform', function() {
+							return "translate(" + width / 2 + "," + height / 2 + ")"
+						}) 
+						.selectAll('text')
+						.data(words)
+						.enter()
+						.append('text')
+						.style('font-size', function(word) { return word.size + 'px'; })
+						.style('fill', function(word, i) { return fill(i); })
+						.attr('text-anchor', 'middle')
+						.attr('transform', function(word) {
+							return "translate(" + [word.x, word.y] + ")"
+						})
+						.text(function(word) { return word.text })
+				}
+			}
+		};
+	});
+});
