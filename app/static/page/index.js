1a0785a03f5f6eb07ec63b6c13a41024fd011cf9
Anthony Li <lizhen.dgut@gmail.com>
2016-02-10 13:13:15 +0800

    添加图表

--- app/static/page/index.js
+++ app/static/page/index.js
@@ -1,5 +1,43 @@
 Vue.config.delimiters = ['${', '}'];
 
+Vue.directive('word-cloud', {
+	params: ['w'],
+	paramWatchers: {
+		w: function(val, oldVal) {
+			var fill = d3.scale.category20();
+
+			var layout = cloud()
+				.size([$("#chart").width(), 500])
+				.words(val)
+				.padding(5)
+				.rotate(function() { return 0; })
+				.font("Impact")
+				.fontSize(function(d) { return d.size; })
+				.on("end", draw);
+			layout.start();
+
+			function draw(words) {
+				d3.select("#chart").append("svg")
+					.attr("width", layout.size()[0])
+					.attr("height", layout.size()[1])
+					.append("g")
+					.attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+					.selectAll("text")
+					.data(words)
+					.enter().append("text")
+					.style("font-size", function(d) { return d.size + "px"; })
+					.style("font-family", "Impact")
+					.style("fill", function(d, i) { return fill(i); })
+					.attr("text-anchor", "middle")
+					.attr("transform", function(d) {
+						return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+					})
+					.text(function(d) { return d.text; });
+			}
+		}
+	}
+});
+
 var datasourceForm = new Vue({
 	el: '#datasource-form',
 	data: {
@@ -8,7 +46,7 @@ var datasourceForm = new Vue({
 	},
 	methods: {
 		submit: function() {
-			debugger;
+			//
 		},
 		refresh: function(spider) {
 			alert(spider);
@@ -16,6 +54,17 @@ var datasourceForm = new Vue({
 	}
 });
 
+var chart = new Vue({
+	el: '#chart',
+	data: {
+		words: []
+	}
+});
+
 $.getJSON('/api/spiders', function(data) {
 	datasourceForm.$set('spiders', data);
+});
+
+$.getJSON('/api/labels', function(data) {
+	chart.$set('words', data);
 });
